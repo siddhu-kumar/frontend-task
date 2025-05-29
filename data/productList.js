aee377eba9362e2d0bfa146dbb035032dc898db7
siddhu-kumar <siddhu8252@gmail.com>
2025-05-29 17:16:11 +0530

    relative path in lists

--- data/productList.js
+++ data/productList.js
@@ -5,19 +5,19 @@ let productsList = [
         productCategory: "Accessories",
         productPrice: 20,
         productCompany: "Google",
-        productCompanyImage: "./image/Google.png",
+        productCompanyImage: "image/Google.png",
         productStatus: true,
-        productImage: "./image/watch.png",
+        productImage: "image/watch.png",
     },
     {
         productId: 2,
         productName: "Mobile",
         productCategory: "Telecommunication",
-        productCompanyImage: "./image/Webflow.png",
+        productCompanyImage: "image/Webflow.png",
         productPrice: 500,
         productCompany: "Webflow",
         productStatus: false,
-        productImage: "./image/mobile.png"
+        productImage: "image/mobile.png"
     },
     {
         productId: 3,
@@ -25,9 +25,9 @@ let productsList = [
         productCategory: "Note Book",
         productPrice: 800,
         productCompany: "Facebook",
-        productCompanyImage: "./image/Facebook.png",
+        productCompanyImage: "image/Facebook.png",
         productStatus: false,
-        productImage: "./image/laptop.png"
+        productImage: "image/laptop.png"
     },
     {
         productId: 4,
@@ -35,29 +35,29 @@ let productsList = [
         productCategory: "Digital",
         productPrice: 250,
         productCompany: "Twitter",
-        productCompanyImage: "./image/Twitter.png",
+        productCompanyImage: "image/Twitter.png",
         productStatus: true,
-        productImage: "./image/tv.png"
+        productImage: "image/tv.png"
     },
     {
         productId: 5,
         productName: "Camera",
         productCategory: "Digital",
         productPrice: 100,
-        productCompanyImage: "./image/Youtube.png",
+        productCompanyImage: "image/Youtube.png",
         productCompany: "YouTube",
         productStatus: false,
-        productImage: "./image/camera.png"
+        productImage: "image/camera.png"
     },
     {
         productId: 6,
         productName: "Perfume",
         productCategory: "Cosmetics",
         productPrice: 25,
-        productCompanyImage: "./image/Reddit.png",
+        productCompanyImage: "image/Reddit.png",
         productCompany: "Reddit",
         productStatus: true,
-        productImage: "./image/perfume.png"
+        productImage: "image/perfume.png"
     },
     {
         productId: 7,
@@ -65,39 +65,39 @@ let productsList = [
         productCategory: "Digital",
         productPrice: 45,
         productCompany: "Spotify",
-        productCompanyImage: "./image/Spotify.png",
+        productCompanyImage: "image/Spotify.png",
         productStatus: false,
-        productImage: "./image/earpod.png"
+        productImage: "image/earpod.png"
     },
     {
         productId: 8,
         productName: "Wireless Charger",
         productCategory: "Digital",
         productPrice: 10,
-        productCompanyImage: "./image/Pinterest.png",
+        productCompanyImage: "image/Pinterest.png",
         productCompany: "Pinterest",
         productStatus: true,
-        productImage: "./image/wirelessCharge.png"
+        productImage: "image/wirelessCharge.png"
     },
     {
         productId: 9,
         productName: "Torch",
         productCategory: "Light",
         productPrice: 20,
-        productCompanyImage: "./image/Twitch.png",
+        productCompanyImage: "image/Twitch.png",
         productCompany: "Twitch",
         productStatus: false,
-        productImage: "./image/torch.png"
+        productImage: "image/torch.png"
     },
     {
         productId: 10,
         productName: "Watch",
         productCategory: "Accessories",
         productPrice: 120,
-        productCompanyImage: "./image/Twitter.png",
+        productCompanyImage: "image/Twitter.png",
         productCompany: "Twitter",
         productStatus: true,
-        productImage: "./image/watch.png"
+        productImage: "image/watch.png"
     },
 ];
 
